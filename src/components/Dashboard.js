b2dc2699a894ef1b2d5dae924c77bfd86f37b7bd
Ayushi <ayjai@microsoft.com>
2019-03-09 13:08:51 -0800

    Add map state to props

---
+++ src/components/Dashboard.js
@@ -0,0 +1,36 @@
+//rcredux can be used to create this
+import React, { Component } from "react";
+import { connect } from "react-redux";
+
+export class Dashboard extends Component {
+  render() {
+    return (
+      <div>
+        <h3>Timeline</h3>
+        <ul className="dashboard-list">
+          {this.props.tweetIds.map(id => (
+            <li key={id}>
+              {" "}
+              <div> Tweet Id: {id}</div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
+
+function mapStateToProps({ tweets }) {
+  return {
+    tweetIds: Object.keys(tweets).sort(
+      (a, b) => tweets[b].timestamp - tweets[a].timestamp
+    )
+  };
+}
+
+const mapDispatchToProps = {};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Dashboard);
