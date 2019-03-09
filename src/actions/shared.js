d7a8b203ddeb0324b8e00f87de58f6cb40d821ab
Ayushi <ayjai@microsoft.com>
2019-03-08 19:45:06 -0800

    Add actions

---
+++ src/actions/shared.js
@@ -0,0 +1,15 @@
+import { getInitialData } from "../utils/api";
+import { receiveUsers } from "./users";
+import { receiveTweets } from "./tweets";
+import { setAuthedUser } from "./authedUser";
+const AUTHED_ID = "ayjai";
+
+export function handleInitialData() {
+  return dispatch => {
+    return getInitialData().then(({ users, tweets }) => {
+      dispatch(receiveUsers(users));
+      dispatch(receiveTweets(tweets));
+      dispatch(setAuthedUser(AUTHED_ID));
+    });
+  };
+}
